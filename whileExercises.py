678f190da43a46c7a878ea80ad5c468aac50728b
Leah Basson <230146740@mycput.ac.za>
2025-04-14 12:10:18 +0200

    :tada:

---
+++ whileExercises.py
@@ -0,0 +1,25 @@
+#Exercise 1
+# for count in range(1,11):
+#     print(count)
+
+#Exercise 2
+# count = 1
+# while count <= 5:
+#     print("Hello")
+#     count += 1
+
+#Exercise 3
+name = input("Enter name: ")
+for character in name:
+    print(character)
+    
+
+#Exercise 4
+# choice = ""
+# while choice != "4":
+#     print("\n--- Menu ---")
+#     print("1. Option A")
+#     print("2. Option B")
+#     print("3. Option C")
+#     print("4. Exit")
+#     choice = input("Enter your choice: (1-4)")
